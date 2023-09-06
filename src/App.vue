d5594f263b1f7fe2d669216148d3068ab1eed8d0
shellwe <benjaminshellwe@outlook.com>
2023-09-06 22:03:07 +0800

    整体.组件.页面.调整 front ver 0.3.4.1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,29 @@
 <template>
   <el-config-provider namespace="ep">
     <BaseHeader />
+
     <div class="flex main-container">
       <BaseSide />
-      <router-view />
+      <div class="vertical-container">
+        <el-page-header class="vertical-item">
+          <template #breadcrumb>
+            <el-breadcrumb separator="/">
+              <el-breadcrumb-item :to="{ path: './' }">
+                面包屑导航栏仍在设计中
+              </el-breadcrumb-item>
+              <el-breadcrumb-item
+              ><a href="./">route 1</a></el-breadcrumb-item
+              >
+              <el-breadcrumb-item>route 2</el-breadcrumb-item>
+            </el-breadcrumb>
+          </template>
+          <template #content>
+            <span class="text-large font-600 mr-3"> Title </span>
+          </template>
+        </el-page-header>
+        <router-view />
+      </div>
+
     </div>
   </el-config-provider>
 </template>
@@ -17,7 +37,13 @@
 .main-container {
   height: calc(100vh - var(--ep-menu-item-height) - 3px);
 }
-
+.vertical-container {
+  display: flex;
+  flex-direction: column; /* 竖向排列 */
+}
+.vertical-item {
+  margin: 10px; /* 可以添加间距 */
+}
 body {
   overflow: hidden;
 }
